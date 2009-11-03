139643d22fdfd761c1edb18865bc0cfccb634301
Geoff Bache <geoff.bache@jeppesen.com>
2009-11-03 21:48:19 +0100

    Change non-instrumented version to be more like instrumented version, make sure dialog.connect works also

--- widgets/file_choosers/save_extra_widgets/target_ui.py
+++ widgets/file_choosers/save_extra_widgets/target_ui.py
@@ -3,6 +3,14 @@
 
 import gtk, os
 
+def dialogRespond(dialog, response, *args):
+    if response == gtk.RESPONSE_OK:
+        print 'Saving', status(checkBox), 'to', fileChooser.get_filename()
+    elif response == gtk.RESPONSE_CANCEL:
+        print 'Closed, nothing will be saved'
+    dialog.destroy()
+    gtk.main_quit()
+
 def status(checkBox):
     if checkBox.get_active():
         return "readonly file"
@@ -20,11 +28,7 @@ dialog.vbox.pack_start(checkBox, expand=False, fill=False)
 dialog.set_default_response(gtk.RESPONSE_OK)
 fileChooser.add_shortcut_folder(os.path.abspath("sampledir/subdir2"))
 fileChooser.set_current_folder(os.path.abspath("sampledir/subdir"))
+dialog.connect("response", dialogRespond)
+dialog.set_modal(True)
 dialog.show_all()
-
-response = dialog.run()
-if response == gtk.RESPONSE_OK:
-    print 'Saving', status(checkBox), 'to', fileChooser.get_filename()
-elif response == gtk.RESPONSE_CANCEL:
-    print 'Closed, nothing will be saved'
-dialog.destroy()
+gtk.main()
