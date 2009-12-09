0aef5c60af48888cd54bb5ba2369badd3a1cd904
Geoff Bache <geoff.bache@jeppesen.com>
2009-12-09 21:22:22 +0100

    Fix non-unique indexing

---
+++ other_ui_features/tree_view_indexing/non_unique_path/target_ui.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+# example basictreeview.py
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+
+class BasicTreeViewExample:
+
+    # close the window and quit
+    def delete_event(self, widget, event, data=None):
+        gtk.main_quit()
+        return False
+
+    def __init__(self):
+        # Create a new window
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+
+        self.window.set_title("Basic TreeView Example")
+
+        self.window.set_size_request(400, 400)
+
+        self.window.connect("delete_event", self.delete_event)
+
+        # create a TreeStore with one string column to use as the model
+        self.treestore = gtk.TreeStore(str)
+
+        self.treestore.append(None, [ "leaf" ])
+        self.treestore.append(None, [ "leaf" ])
+
+        # create the TreeView using treestore
+        self.treeview = gtk.TreeView(self.treestore)
+
+        # create the TreeViewColumn to display the data
+        self.tvcolumn = gtk.TreeViewColumn('Column 0')
+
+        # add tvcolumn to treeview
+        self.treeview.append_column(self.tvcolumn)
+
+        # create a CellRendererText to render the data
+        self.cell = gtk.CellRendererText()
+
+        # add the cell to the tvcolumn and allow it to expand
+        self.tvcolumn.pack_start(self.cell, True)
+
+        # set the cell "markup" attribute to column 0 - retrieve marked-up text
+        # from that column in treestore
+        self.tvcolumn.add_attribute(self.cell, 'text', 0)
+
+        # make it searchable
+        self.treeview.get_selection().set_mode(gtk.SELECTION_MULTIPLE)
+        self.treeview.get_selection().connect("changed", self.selectionChanged)
+        self.treeview.expand_all()
+
+        self.window.add(self.treeview)
+
+        self.window.show_all()
+
+    def selectionChanged(self, selection, *args):
+        print "There are now", selection.count_selected_rows(), "rows selected."
+        
+
+def main():
+    gtk.main()
+
+if __name__ == "__main__":
+    tvexample = BasicTreeViewExample()
+    main()
