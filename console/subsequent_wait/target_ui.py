fdbb1b439264100ccf3595271d9027d4206d2a83
Geoff Bache <geoff.bache@jeppesen.com>
2009-12-13 21:45:35 +0100

    Subsequent waits are handled somewhat differently

---
+++ console/subsequent_wait/target_ui.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from usecase import applicationEvent
+import time, signal
+
+def handleSignal(signum, *args):
+    print "Got signal", signum
+
+signal.signal(signal.SIGQUIT, handleSignal)
+signal.signal(signal.SIGINT, handleSignal)
+
+time.sleep(0.5)
+applicationEvent("first sleep to complete")
+time.sleep(5)
+applicationEvent("second sleep to complete")
+time.sleep(5)
