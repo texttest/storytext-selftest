320c5230242887f3c689929acb058659fa48b9b4
Geoff Bache <geoff.bache@jeppesen.com>
2009-12-17 21:14:24 +0100

    New test for gtk.CellView

---
+++ other_ui_features/autologging/cellview/target_ui.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import time
+import pygtk
+pygtk.require('2.0')
+import gtk
+
+class Example:
+    def __init__(self):
+        window = gtk.Window()
+        window.set_title("Cellview Example")
+        window.connect('destroy', lambda w: gtk.main_quit())
+        cellview = gtk.CellView()
+        model = gtk.ListStore(str)
+        model.append([ "Cell View Text "])
+        cell = gtk.CellRendererText()
+        cellview.set_model(model)
+        cellview.pack_start(cell)
+        cellview.set_attributes(cell, text=0)
+        cellview.set_displayed_row((0,))
+        window.add(cellview)
+        window.show_all()
+        return
+
+def main():
+    gtk.main()
+    return
+
+if __name__ == "__main__":
+    ee = Example()
+    main()
