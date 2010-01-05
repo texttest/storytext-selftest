5008c54d6c6ce168b7b3a37384af5839a5931156
Geoff Bache <geoff.bache@jeppesen.com>
2010-01-05 22:45:47 +0100

    New test for application events in a UI

---
+++ other_ui_features/ui_appevent/target_ui.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+# ZetCode PyGTK tutorial 
+#
+# This example shows a toolbar
+# widget
+#
+# author: jan bodnar
+# website: zetcode.com 
+# last edited: February 2009
+
+
+import gtk
+from usecase import applicationEvent
+
+class PyApp(gtk.Window):
+
+    def __init__(self):
+        super(PyApp, self).__init__()
+
+        self.set_title("App event")
+        applicationEvent("application to start")
+        self.connect("destroy", gtk.main_quit)
+        self.show_all()
+        
+       
+PyApp()
+gtk.main()
