f13374374d1de3ea053994d09049beaeea67b80f
Geoff Bache <geoff.bache@jeppesen.com>
2010-02-26 11:42:49 +0100

    Try to replace auto recording even when the application throws an exception such as SystemExit

---
+++ tkinter/system_exit/target_ui.py
@@ -0,0 +1,23 @@
+# File: hello2.py
+
+from Tkinter import *
+import sys
+
+class App:
+
+    def __init__(self, master):
+
+        frame = Frame(master)
+        frame.pack()
+
+        self.button = Button(frame, text="QUIT", fg="red", command=frame.quit)
+        self.button.pack(side=LEFT)
+
+
+root = Tk()
+
+app = App(root)
+
+root.mainloop()
+# Shouldn't prevent the usecase name chooser appearing...
+sys.exit(1)
