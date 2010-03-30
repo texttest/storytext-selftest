05a7c12d993800d1f7d43f15a0c7d17d942d6820
Geoff Bache <geoff.bache@jeppesen.com>
2010-03-30 17:05:52 +0200

    Added logging support for LabelFrame, update a few tests missed previously

--- tkinter/checkbutton/target_ui.py
+++ tkinter/checkbutton/target_ui.py
@@ -3,15 +3,17 @@
 from Tkinter import *
 
 top = Tk()
+lf1 = LabelFrame(top, text="Settings", padx=6, pady=6)
 CheckVar1 = IntVar()
 CheckVar2 = IntVar()
-C1 = Checkbutton(top, text = "Music", variable = CheckVar1, \
+C1 = Checkbutton(lf1, text = "Music", variable = CheckVar1, \
                  onvalue = 1, offvalue = 0, height=5, \
                  width = 20)
-C2 = Checkbutton(top, text = "Video", variable = CheckVar2, \
+C2 = Checkbutton(lf1, text = "Video", variable = CheckVar2, \
                  onvalue = 1, offvalue = 0, height=5, \
                  width = 20)
 C2.select()
 C1.pack()
 C2.pack()
+lf1.pack()
 top.mainloop()
