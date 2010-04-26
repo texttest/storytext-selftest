6aed603fb0378fba050618c08e48dd851dd0b1ff
Geoff Bache <geoff.bache@jeppesen.com>
2010-04-26 12:30:15 +0200

    Do things from mainloop now, cannot import and call globally or interception is bypassed

--- tkinter/menu_and_text/target_ui.py
+++ tkinter/menu_and_text/target_ui.py
@@ -1,18 +1,18 @@
 # menu-example-3.py
 
-from Tkinter import *
+import Tkinter
 from ScrolledText import ScrolledText
 
-root = Tk()
+root = Tkinter.Tk()
 
 def hello():
     print "hello"
-    text.insert(END, "Did something from a menu!\n")
+    text.insert(Tkinter.END, "Did something from a menu!\n")
 
-menubar = Menu(root)
+menubar = Tkinter.Menu(root)
 
 # create a pulldown menu, and add it to the menu bar
-filemenu = Menu(menubar, tearoff=0)
+filemenu = Tkinter.Menu(menubar, tearoff=0)
 filemenu.add_command(label="Open", command=hello)
 filemenu.add_command(label="Save", command=hello)
 filemenu.add_separator()
@@ -20,21 +20,21 @@ filemenu.add_command(label="Exit", command=root.quit)
 menubar.add_cascade(label="File", menu=filemenu)
 
 # create more pulldown menus
-editmenu = Menu(menubar, tearoff=0)
+editmenu = Tkinter.Menu(menubar, tearoff=0)
 editmenu.add_command(label="Cut", command=hello)
 editmenu.add_command(label="Copy", command=hello)
 editmenu.add_command(label="Paste", command=hello)
 menubar.add_cascade(label="Edit", menu=editmenu)
 
-helpmenu = Menu(menubar, tearoff=0)
+helpmenu = Tkinter.Menu(menubar, tearoff=0)
 helpmenu.add_command(label="About", command=hello)
 menubar.add_cascade(label="Help", menu=helpmenu)
 
 # display the menu
 root.config(menu=menubar)
 
-Label(root, text="Some text:").grid(row=0, column=0)
+Tkinter.Label(root, text="Some text:").grid(row=0, column=0)
 text = ScrolledText(root)
 text.grid(row=0, column=1)
 
-mainloop()
+Tkinter.mainloop()
