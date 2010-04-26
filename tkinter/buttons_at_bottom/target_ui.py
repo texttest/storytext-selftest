53235eb60b58e18f6b7d456c311d3ea8bfcde826
Geoff Bache <geoff.bache@jeppesen.com>
2010-04-26 20:47:13 +0200

    Make sure items at the bottom reflect this in the output

---
+++ tkinter/buttons_at_bottom/target_ui.py
@@ -0,0 +1,28 @@
+# File: hello2.py
+
+from Tkinter import *
+
+class App:
+
+    def __init__(self, master):
+
+        frame = Frame(master)
+        frame.pack()
+
+        label = Label(frame, text="A blue label", bg="blue")
+        label.pack(side=BOTTOM)
+
+        self.button = Button(frame, text="QUIT", fg="red", command=frame.quit)
+        self.button.pack(side=LEFT)
+
+        self.hi_there = Button(frame, name="hello button", text="Hello", command=self.say_hi)
+        self.hi_there.pack(side=LEFT)
+
+    def say_hi(self):
+        print "hi there, everyone!"
+
+root = Tk()
+
+app = App(root)
+
+root.mainloop()
