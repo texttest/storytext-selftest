697cd44f82e7f8f3f20cde64dedd3ffbd65bb6f2
Geoff Bache <geoff.bache@jeppesen.com>
2010-05-07 15:49:00 +0200

    Add a Canvas window as well

--- tkinter/widgets/canvas/target_ui.py
+++ tkinter/widgets/canvas/target_ui.py
@@ -8,7 +8,7 @@ def describe(event):
     print "Clicked item", item
 
 root = Tk()
-canvas = Canvas(root, width=300, height=300)
+canvas = Canvas(root, width=300, height=320)
 canvas.pack(fill=BOTH)
 square = canvas.create_rectangle(0,0,150,150, fill="green", tags="rectangle")
 circle = canvas.create_oval(0,150,150,300, fill="blue")
@@ -16,6 +16,8 @@ diamond = canvas.create_polygon(150,75,225,0,300,75,225,150, fill="red", tags="1
 text = canvas.create_text(230,230, text="Tkinter canvas", fill="purple", 
                           font=("Helvetica", "16"))
 label = canvas.create_text(0,75, text="Green rectangle", anchor=W, tags="rectangle")
+button = Button(root, text="A button")
+canvas.create_window(0, 300, window=button, anchor=NW)
 canvas.bind('<Button-1>', describe)
 
 root.mainloop()
