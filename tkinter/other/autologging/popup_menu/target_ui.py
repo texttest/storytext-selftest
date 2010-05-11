be081881d128c96705bdd94049debc7b54712663
Geoff Bache <geoff.bache@jeppesen.com>
2010-05-11 11:09:12 +0200

    Post descriptions of popup submenus also

--- tkinter/other/autologging/popup_menu/target_ui.py
+++ tkinter/other/autologging/popup_menu/target_ui.py
@@ -15,9 +15,12 @@ def home(*args):
     print "Home"
 
 # create a menu
-popup = Menu(root, tearoff=0)
+popup = Menu(root, name="popup menu", tearoff=0)
 popup.add_command(label="Next", command=next)
 popup.add_command(label="Previous", command=previous)
+submenu = Menu(popup, tearoff=0)
+submenu.add_command(label="Next again", command=next)
+popup.add_cascade(label="Submenu", menu=submenu)
 popup.add_separator()
 popup.add_command(label="Home", command=home)
 
