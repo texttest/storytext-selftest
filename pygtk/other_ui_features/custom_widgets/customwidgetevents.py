b1454537a96235040bcf040e20f5d0d07ebc4634
Geoff Bache <geoff.bache@jeppesen.com>
2010-07-30 21:55:05 +0200

    Add a hook to attach support for custom widgets

---
+++ pygtk/other_ui_features/custom_widgets/customwidgetevents.py
@@ -0,0 +1,21 @@
+
+from gtkusecase.baseevents import SignalEvent
+import gtk
+
+class MyButtonEvent(SignalEvent):
+    def generate(self, *args):
+        print "We faked a button click!"
+        SignalEvent.generate(self, *args)
+
+    def shouldRecord(self, *args):
+        print "Refusing to record the button click!"
+        return False
+
+class InsertEvent(SignalEvent):
+    signalName = "row-inserted"
+    def connectRecord(self, method):
+        self._connectRecord(self.widget.get_model(), method)
+
+# Standard name for module containing custom widget events
+customEventTypes = [(gtk.Button, [ MyButtonEvent ]),
+                    (gtk.TreeView, [ InsertEvent])]
