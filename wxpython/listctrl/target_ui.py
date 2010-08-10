f3821db225a71879fc0259e53196101582a2e2d7
xuan <xuan@lisa>
2010-08-10 20:58:19 +0100

    simplifed target_ui.py

--- wxpython/listctrl/target_ui.py
+++ wxpython/listctrl/target_ui.py
@@ -5,23 +5,22 @@
 import wx
 
 APP_SIZE_X = 175
-APP_SIZE_Y = 125
+APP_SIZE_Y = 150
 
 class MainFrame(wx.Frame):
     def __init__(self, parent, id):
         wx.Frame.__init__(self, parent, id, 'wx.ListCtrl', size=(APP_SIZE_X, APP_SIZE_Y)) 
 
-        self.lc = wx.ListCtrl(self, -1, style=wx.LC_REPORT)
-        self.lc.InsertColumn(0, 'number')
-        self.lc.InsertColumn(1, 'english')
-        i = 0
-        for item in ["one", "two", "three", "four"]:
-            self.lc.InsertStringItem(i, str(i))
-            self.lc.SetStringItem(i, 1, item)
-            i += 1
+        self.lc = wx.ListCtrl(self, 1, style=wx.LC_REPORT)
+        self.lc.InsertColumn(0, 'numbers')
+        for label in ["zero", "one", "two", "three", "four"]:
+            item = wx.ListItem()
+            item.SetText(label)
+            self.lc.InsertItem(item)
+        self.Bind(wx.EVT_LIST_ITEM_SELECTED, self.OnSelect, id=1)
 
-    def OnClose(self, event):
-        self.Close()
+    def OnSelect(self, event):
+        print 'Selected', self.lc.GetItemText(event.GetIndex())
 
 class MainApp(wx.App):
     """Class Main App."""
