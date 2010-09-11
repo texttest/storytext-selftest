4d57a9761f817d1978c353a810d2cb65d9cfc37e
Geoff Bache <geoff.bache@jeppesen.com>
2010-09-11 20:35:22 +0200

    New test for integer responses to dialogs, showing we don't really handle it

---
+++ pygtk/widgets/dialog_int_responses/target_ui.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+import gtk
+
+class AboutDialog():
+    
+    BUG_BUTTON = 1
+    DONATE_BUTTON = 2
+    CLOSE_BUTTON = 3
+    
+    def __init__(self, parent_window):
+        self._parent_window = parent_window
+
+    def show(self):
+        dialog = gtk.Dialog('The Dialog Title', self._parent_window)
+        text = "Some text"
+        about_label = gtk.Label(text)
+        dialog.vbox.add(about_label)
+        
+        dialog.add_button('File a bug', AboutDialog.BUG_BUTTON)
+        dialog.add_button('Donate', AboutDialog.DONATE_BUTTON)
+        dialog.add_button('Close', AboutDialog.CLOSE_BUTTON)
+        
+        self._runDialog(dialog)
+        
+    def _responseCallback(self, dialog, result):
+        dialog.hide()
+        print "Result was", result    
+
+    def _runDialog(self, dialog):
+        dialog.connect("response", self._responseCallback)
+        dialog.show_all()
+
+
+if __name__ == "__main__":
+    win = gtk.Window()
+    dialog = AboutDialog(win)
+
+    def clicked(*args):
+        global dialog
+        dialog.show()
+    box = gtk.VBox()
+    button = gtk.Button("hi - this button opens the about dialog")
+    button.connect("clicked", clicked)
+    box.add(button)
+
+    win.set_deletable(True)
+
+    win.connect("destroy", gtk.main_quit, None)
+
+    win.add(box)
+
+    win.show_all()
+    gtk.main()
