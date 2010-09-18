b84536f4831c593e0b45ec18175d1aa8c84184a6
xuan <xuan@lisa>
2010-09-18 23:53:45 +0100

    added a test case for dialog. the results are not consistent in normal speed and slow motion yet.

---
+++ wxpython/widgets/dialog/test_dialog.py
@@ -0,0 +1,43 @@
+
+import unittest
+import wx
+
+class MyDialog(wx.Dialog):
+    def __init__(self, parent):
+        wx.Dialog.__init__(self, parent, -1, 'Test')
+        wx.Button(self, wx.ID_OK)
+
+class TestMyDialog(unittest.TestCase):
+
+    def setUp(self):
+        self.app = wx.App()
+        self.frame = wx.Frame(None)
+        self.frame.Show()
+
+    def tearDown(self):
+        wx.CallAfter(self.app.Exit)
+        self.app.MainLoop()
+
+    def testDialog(self):
+        def clickOK():
+            clickEvent = wx.CommandEvent(wx.wxEVT_COMMAND_BUTTON_CLICKED, wx.ID_OK)
+            self.dlg.ProcessEvent(clickEvent)
+        wx.CallAfter(clickOK)
+        self.ShowDialog()
+
+    def ShowDialog(self):
+        self.dlg = MyDialog(self.frame)
+        self.dlg.ShowModal()
+        self.dlg.Destroy()
+
+def main():
+    app = wx.App()
+    app.MainLoop()
+    frame = wx.Frame(None)
+    frame.Show()
+    dlg = MyDialog(frame)
+    dlg.ShowModal()
+    dlg.Destroy()
+
+if __name__ == '__main__':
+    unittest.main()
