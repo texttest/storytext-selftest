d1d9585460ae65022175f8f25f2c8261551a5e07
Geoff Bache <geoff.bache@jeppesen.com>
2010-12-21 17:19:50 +0100

    Making close confirmation dialogs work

---
+++ swt/closure_confirm/target_ui.py
@@ -0,0 +1,39 @@
+from org.eclipse.swt import *
+from org.eclipse.swt.widgets import *
+from org.eclipse.swt.layout import *
+
+shell = Shell()
+
+class ConfirmListener(Listener):
+    def handleEvent(self, e):
+        style = SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL
+        messageBox = Shell(shell, style)
+        messageBox.setText("Information")
+        layout = GridLayout()
+        messageBox.setLayout(layout)
+        label = Label(messageBox, SWT.NONE)
+        label.setText("You're exiting!")
+        button = Button(messageBox, SWT.PUSH)
+        button.setText("Sure")
+        class CloseListener(Listener):
+            def handleEvent(self, e):
+                messageBox.close()
+        button.addListener(SWT.Selection, CloseListener())
+        messageBox.open()
+        messageBox.pack()
+        display = messageBox.getDisplay()
+        while not messageBox.isDisposed():
+            if not display.readAndDispatch():
+                display.sleep()
+
+shell.setText("Main Window")
+shell.addListener(SWT.Close, ConfirmListener())
+
+shell.setSize(200, 200)
+shell.open()
+display = shell.getDisplay()
+while not shell.isDisposed():
+    if not display.readAndDispatch():
+        display.sleep()
+        
+display.dispose()
