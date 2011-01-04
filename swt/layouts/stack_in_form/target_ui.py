8ecfd8e03614d3e306b42c913c289561f7cd1753
Geoff Bache <geoff.bache@jeppesen.com>
2011-01-04 11:41:09 +0100

    Describe stacked layouts and the changes in them

--- swt/layouts/stack_in_form/target_ui.py
+++ swt/layouts/stack_in_form/target_ui.py
@@ -30,13 +30,23 @@ shell.setBounds(10, 10, 300, 200)
 
 # create the button that will switch between the pages
 pageButton = Button(shell, SWT.PUSH)
-pageButton.setText("Push")
+pageButton.setText("Change Page")
 pageButton.setBounds(10, 10, 80, 25)
 size = pageButton.computeSize(SWT.DEFAULT, SWT.DEFAULT)
 pageButtonData = FormData(size.x, SWT.DEFAULT)
 pageButtonData.right = FormAttachment(100, -4)
 pageButtonData.bottom = FormAttachment(100, 0)
 pageButton.setLayoutData(pageButtonData)
+
+# create the button that will add a page
+addButton = Button(shell, SWT.PUSH)
+addButton.setText("Add Page")
+addButton.setBounds(10, 10, 80, 25)
+size = addButton.computeSize(SWT.DEFAULT, SWT.DEFAULT)
+addButtonData = FormData(size.x, SWT.DEFAULT)
+addButtonData.left = FormAttachment(0, 0)
+addButtonData.bottom = FormAttachment(100, 0)
+addButton.setLayoutData(addButtonData)
 
 #create the composite that the pages will share
 contentPanel = Composite(shell, SWT.BORDER)
@@ -61,21 +71,24 @@ layout.topControl = page0
 page1 = Composite(contentPanel, SWT.NONE)
 page1.setLayout(RowLayout())
 contentPanel.layout()
-page1.setVisible(True)
 
 class FlipListener(Listener):
     def handleEvent(self, e):
         if layout.topControl is page0:
-            text = Text(page1, SWT.NONE)
-            text.setText("Text on page 2")
-            text.pack()
-            text.setVisible(True)
             layout.topControl = page1
         else:
             layout.topControl = page0
         contentPanel.layout()
 
-
+class AddListener(Listener):
+    def handleEvent(self, e):
+        text = Text(page1, SWT.NONE)
+        text.setText("Text on page 2")
+        text.setVisible(True)
+        text.pack()
+        page1.setVisible(True)
+            
+addButton.addListener(SWT.Selection, AddListener())
 pageButton.addListener(SWT.Selection, FlipListener())
 shell.open()
 while not shell.isDisposed():
