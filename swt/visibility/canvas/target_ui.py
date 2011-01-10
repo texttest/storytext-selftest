9f8c322fcb761c36e4a4946f111a84f20e7ca3ea
Geoff Bache <geoff.bache@jeppesen.com>
2011-01-10 13:38:38 +0100

    New test for describing Canvases and changes to them

---
+++ swt/visibility/canvas/target_ui.py
@@ -0,0 +1,52 @@
+## /*******************************************************************************
+##  * Copyright (c) 2000, 2004 IBM Corporation and others.
+##  * All rights reserved. This program and the accompanying materials
+##  * are made available under the terms of the Eclipse Public License v1.0
+##  * which accompanies this distribution, and is available at
+##  * http://www.eclipse.org/legal/epl-v10.html
+##  *
+##  * Contributors:
+##  *     IBM Corporation - initial API and implementation
+##  *******************************************************************************/
+## /*
+##  * Tree example snippet: create a tree
+##  *
+##  * For a list of all SWT example snippets see
+##  * http://www.eclipse.org/swt/snippets/
+## */
+from org.eclipse.swt import *
+from org.eclipse.swt.widgets import *
+from org.eclipse.swt.layout import *
+
+display = Display()
+shell = Shell(display)
+shell.setLayout(FillLayout())
+
+bar = Menu(shell, SWT.BAR)
+shell.setMenuBar(bar)
+fileItem = MenuItem(bar, SWT.CASCADE)
+fileItem.setText("&File")
+submenu = Menu(shell, SWT.DROP_DOWN)
+fileItem.setMenu(submenu)
+item = MenuItem(submenu, SWT.PUSH)
+item.setText("Change Colour")
+
+red = display.getSystemColor(SWT.COLOR_RED)
+blue = display.getSystemColor(SWT.COLOR_BLUE)
+c = Canvas(shell, SWT.BORDER)
+c.setBackground(red)
+c.setFocus()
+
+class ShowListener(Listener):
+    def handleEvent(self, e):
+        c.setBackground(blue)
+        
+item.addListener(SWT.Selection, ShowListener())
+
+shell.setSize(200, 200)
+shell.open()
+while not shell.isDisposed():
+    if not display.readAndDispatch():
+        display.sleep()
+
+display.dispose()
