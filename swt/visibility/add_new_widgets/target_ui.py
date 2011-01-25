52a348f8e1569f96df0f7cbf1a36eff7afdfd2b3
Geoff Bache <geoff.bache@jeppesen.com>
2011-01-25 14:19:07 +0100

    Generalising visibility code to pick up anything caused by Paint events

---
+++ swt/visibility/add_new_widgets/target_ui.py
@@ -0,0 +1,57 @@
+## /*******************************************************************************
+##  * Copyright (c) 2000, 2004 IBM Corporation and others.
+##  * All rights reserved. This program and the accompanying materials
+##  * are made available under the terms of the Eclipse Public License v1.0
+##  * which accompanies this distribution, and is available at
+##  * http://www.eclipse.org/legal/epl-v10.html
+##  *
+##  * Contributors:
+##  *     IBM Corporation - initial API and implementation
+##  *******************************************************************************/
+## /*
+##  * Tree example snippet: create a tree
+##  *
+##  * For a list of all SWT example snippets see
+##  * http://www.eclipse.org/swt/snippets/
+## */
+from org.eclipse.swt import *
+from org.eclipse.swt.widgets import *
+from org.eclipse.swt.layout import *
+
+display = Display()
+shell = Shell(display)
+shell.setLayout(FillLayout())
+
+bar = Menu(shell, SWT.BAR)
+shell.setMenuBar(bar)
+fileItem = MenuItem(bar, SWT.CASCADE)
+fileItem.setText("&File")
+submenu = Menu(shell, SWT.DROP_DOWN)
+fileItem.setMenu(submenu)
+item = MenuItem(submenu, SWT.PUSH)
+item.setText("Add new row")
+
+composite = Composite(shell, SWT.NONE)
+layout = GridLayout(2, False)
+composite.setLayout(layout)
+
+label = Label(composite, SWT.NONE)
+label.setText("Name")
+text = Text(composite, SWT.NONE)
+
+class AddListener(Listener):
+    def handleEvent(self, e):
+        label2 = Label(composite, SWT.NONE)
+        label2.setText("City")
+        text2 = Text(composite, SWT.NONE)
+        shell.pack()
+                
+item.addListener(SWT.Selection, AddListener())
+
+shell.setSize(200, 200)
+shell.open()
+while not shell.isDisposed():
+    if not display.readAndDispatch():
+        display.sleep()
+
+display.dispose()
