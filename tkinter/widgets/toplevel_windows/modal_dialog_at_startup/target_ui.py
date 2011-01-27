b291e42e2a2f787600d9dc3973936028e3b26f37
Geoff Bache <geoff.bache@jeppesen.com>
2011-01-27 11:00:48 +0100

    New test for modal dialogs appearing at startup

---
+++ tkinter/widgets/toplevel_windows/modal_dialog_at_startup/target_ui.py
@@ -0,0 +1,30 @@
+# File: hello2.py
+
+from Tkinter import *
+import tkMessageBox
+
+class App:
+
+    def __init__(self, master):
+        self.master = master
+        self.frame = Frame(master)
+        self.frame.pack()
+
+        self.button = Button(self.frame, text="QUIT", fg="red", command=self.frame.quit)
+        self.button.pack(side=LEFT)
+
+        win = Toplevel(self.master)
+	win.title('Hi!')
+        win.grab_set()
+	def destroy(*args):
+            win.destroy()
+        label = Label(win, name="hello label", text="hi there, everyone!", bg="white")
+        label.bind("<Button-1>", destroy)
+	label.pack()
+        win.wait_window(win)
+
+root = Tk()
+
+app = App(root)
+
+root.mainloop()
