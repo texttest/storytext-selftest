82645f28a1991ba666de480463e04c32866d6a50
Geoff Bache <geoff.bache@jeppesen.com>
2011-02-11 10:52:14 +0100

    Made DateTime test more complex, changed date formatter, fixed bug with times PM

--- swt/datetime/target_ui.py
+++ swt/datetime/target_ui.py
@@ -3,15 +3,23 @@ from org.eclipse.swt.widgets import *
 from org.eclipse.swt.layout import *
 
 shell = Shell()
-shell.setLayout(RowLayout())
+shell.setLayout(GridLayout(2, False))
 
 calendar = DateTime(shell, SWT.CALENDAR)
 calendar.setData("org.eclipse.swtbot.widget.key", "The Calendar")
 # Month starts from 0, oh yes!
 calendar.setDate(1975, 1, 18)
-time = DateTime(shell, SWT.TIME)
+gridData = GridData()
+gridData.horizontalSpan = 2
+calendar.setLayoutData(gridData)
+
+date = DateTime(shell, SWT.DATE)
+date.setData("org.eclipse.swtbot.widget.key", "The Date")
+date.setDate(1976, 1, 3)
+
+time = DateTime(shell, SWT.TIME | SWT.SHORT)
 time.setData("org.eclipse.swtbot.widget.key", "The Time")
-time.setTime(4, 0, 0)
+time.setTime(14, 0, 0)
     
 class PrintListener(Listener):
     def handleEvent(self, e):
@@ -19,10 +27,16 @@ class PrintListener(Listener):
 
 class PrintListener2(Listener):
     def handleEvent(self, e):
-        print "TimeChanged"
+        print "Time changed"
+
+class PrintListener3(Listener):
+    def handleEvent(self, e):
+        print "Date changed"
+
 
 calendar.addListener(SWT.Selection, PrintListener())
 time.addListener(SWT.Selection, PrintListener2())
+date.addListener(SWT.Selection, PrintListener3())
 
 shell.pack()
 shell.open()
