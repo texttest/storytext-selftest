fda8bcf21d186c7fceb4aa5b6a1e62b154f867df
Daniel Galdames <daniel.galdames@jeppesen.com>
2011-04-21 17:31:28 +0200

    java swing first commit

---
+++ swing/radio_buttons/target_ui.py
@@ -0,0 +1,33 @@
+from javax import swing
+from java.awt import BorderLayout
+
+class RadioButtonApp:
+        
+    def make_ui(self):
+        frame = swing.JFrame("Radio button demo")
+        frame.setDefaultCloseOperation(swing.JFrame.DISPOSE_ON_CLOSE)
+        frame.setLayout(BorderLayout())
+        rButton1 = swing.JRadioButton("First", actionPerformed=self.printButton)
+        rButton2 = swing.JRadioButton("Second", actionPerformed=self.printButton)
+        newContentPane = swing.JPanel()
+        newContentPane.add(rButton1)
+        newContentPane.add(rButton2)
+        buttonGroup = swing.ButtonGroup()
+        buttonGroup.add(rButton1)
+        buttonGroup.add(rButton2)
+        frame.add(newContentPane)
+        frame.pack()
+        frame.setVisible(True)
+
+    def printButton(self, event):
+        widget = event.getSource()
+        if widget.isSelected():
+            print "Selected the", widget.getText(), "button"
+
+    def main():
+        app = RadioButtonApp()
+        app.make_ui()
+    main = staticmethod(main)
+
+RadioButtonApp.main()
+
---
+++ swing/tabs/target_ui.py
@@ -0,0 +1,36 @@
+from javax import swing
+from java.awt import BorderLayout, Dimension
+from java.awt.event import KeyEvent
+from java.lang import System
+class TabsApp:            
+    def make_ui(self):
+        frame = swing.JFrame("Tabs demo")
+        frame.setDefaultCloseOperation(swing.JFrame.DISPOSE_ON_CLOSE)
+        frame.setLayout(BorderLayout())
+        frame.setPreferredSize(Dimension(300, 250))
+        frame.add(self.createTabbedPane())
+        frame.pack()
+        frame.setVisible(True)
+
+    def createTabbedPane(self):
+        class TabSelectionListener(swing.event.ChangeListener):
+            def stateChanged(listenerSelf, event):
+                pane = event.getSource()
+                index = pane.getSelectedIndex()
+                print pane.getTitleAt(index), "selected"
+                
+        tPane = swing.JTabbedPane(swing.JTabbedPane.TOP)
+        self.addTabs(tPane)
+        tPane.addChangeListener(TabSelectionListener())
+        return tPane
+    
+    def addTabs(self, tabbedPane):
+        for i in range(1, 6):
+            tabbedPane.add("Tab " + str(i), swing.JPanel())
+            
+    def main():
+        app = TabsApp()
+        app.make_ui()
+    main = staticmethod(main)
+
+TabsApp.main()
