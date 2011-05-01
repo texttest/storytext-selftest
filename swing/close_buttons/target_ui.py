dbf58c70d974c4fb5cd4a0e2543fb332bb99b8ba
Daniel Galdames <daniel.galdames@jeppesen.com>
2011-05-01 17:37:35 +0200

    Dialog and close buttons tests added. ui_map.conf files updated

---
+++ swing/close_buttons/target_ui.py
@@ -0,0 +1,33 @@
+from javax import swing
+from java.awt import BorderLayout
+from java.awt.event import WindowAdapter
+
+class CloseButtonsApp:
+    def make_ui(self):
+        self.frame = swing.JFrame("Close Buttons")
+        self.frame.setDefaultCloseOperation(swing.JFrame.DISPOSE_ON_CLOSE)
+        self.frame.setLayout(BorderLayout())
+        self.frame.setSize(200, 200);
+        button1 = swing.JButton("Why?", actionPerformed=self.printMessage)
+        self.button2 = swing.JButton("Exit",actionPerformed=self.close)
+        self.button2.setEnabled(False)
+        panel = swing.JPanel()
+        panel.add(button1)
+        panel.add(self.button2)
+        self.frame.add(panel)
+        self.frame.pack()
+        self.frame.setVisible(True)
+    
+    def close(self, event):
+        self.frame.dispose()
+
+    def printMessage(self, event):
+        print "Because"
+        self.button2.setEnabled(True)
+
+    def main():
+        app = CloseButtonsApp()
+        app.make_ui()
+    main = staticmethod(main)
+
+CloseButtonsApp.main()
---
+++ swing/dialogs/confirm/target_ui.py
@@ -0,0 +1,32 @@
+from javax import swing
+from java.awt import BorderLayout, Dimension
+from java.awt.event import KeyEvent, WindowAdapter
+from java.lang import System, Runnable, Thread
+from javax.swing import JOptionPane, SwingUtilities
+
+class ConfirmDialogApp:                  
+    def make_ui(self):                
+        self.frame = swing.JFrame("Confirm Dialog Demo")
+        self.frame.setDefaultCloseOperation(swing.JFrame.DISPOSE_ON_CLOSE)
+        self.frame.setLayout(BorderLayout())
+        panel = swing.JPanel()
+        button = swing.JButton("Exit", actionPerformed=self.quitDialog)
+        panel.add(button)
+        self.frame.add(panel)
+        self.frame.pack()
+        self.frame.setVisible(True)
+
+    def quitDialog(self, event):
+        confirmed = JOptionPane.showConfirmDialog(self.frame, 
+                                                    "Are you sure you want to exit?", 
+                                                    "User Confirmation", 
+                                                    JOptionPane.YES_NO_OPTION)
+        if confirmed == JOptionPane.YES_OPTION:
+            self.frame.dispose()
+                    
+    def main():
+        app = ConfirmDialogApp()
+        app.make_ui()
+    main = staticmethod(main)
+
+ConfirmDialogApp.main()
