481428136c71d34ecd10b0177767c6d3afa03cb1
Daniel Galdames <daniel.galdames@jeppesen.com>
2011-05-27 10:45:36 +0200

    3 new table test added

---
+++ swing/tables/select_no_editable_cells/target_ui.py
@@ -0,0 +1,43 @@
+from javax import swing
+from java.awt import BorderLayout, Dimension
+from java.awt.event import KeyEvent
+
+class TableApp:
+        
+    def make_ui(self):
+        frame = swing.JFrame("Table demo")
+        frame.setDefaultCloseOperation(swing.JFrame.DISPOSE_ON_CLOSE)
+        frame.setLayout(BorderLayout())
+        scrollPane = swing.JScrollPane()
+        scrollPane.setPreferredSize(Dimension(300,100))
+        scrollPane.getViewport().setView(self.createTable())
+        panel = swing.JPanel()
+        panel.add(scrollPane)
+        frame.add(panel)
+        frame.pack()
+        frame.setVisible(True)
+
+    def createTable(self):
+        class myTable(swing.JTable):
+            def isCellEditable(self, rowIndex, colIndex):
+                return False
+    
+        data = [ ['Tom'], ['Dick'], ['Harry'] ]
+        columns = ("Name",)
+        model = swing.table.DefaultTableModel(data, columns)
+        table = myTable(model)
+        table.setSelectionMode(swing.ListSelectionModel.MULTIPLE_INTERVAL_SELECTION)
+        table.setCellSelectionEnabled(True)
+        return table
+            
+    def printCells(self, event):
+        widget = event.getSource()
+        print "Selected Tool bar's",widget.getText(), "button"
+
+    @staticmethod            
+    def main():
+        app = TableApp()
+        app.make_ui()
+
+TableApp.main()
+
