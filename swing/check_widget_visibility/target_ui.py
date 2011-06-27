4584274d6320543224c1e367def00bc9f0bc6315
Daniel Galdames <daniel.galdames@jeppesen.com>
2011-06-27 15:11:38 +0200

    Test for widget visibility added

---
+++ swing/check_widget_visibility/target_ui.py
@@ -0,0 +1,40 @@
+from javax import swing
+from java.awt import BorderLayout, Dimension
+from java.awt.event import KeyEvent
+from java.lang import System
+class TabsApp:            
+    def make_ui(self):
+        frame = swing.JFrame("Tabs demo")
+        frame.setDefaultCloseOperation(swing.JFrame.DISPOSE_ON_CLOSE)
+        frame.setLayout(BorderLayout())
+        frame.setPreferredSize(Dimension(300, 250))
+        frame.add(self.createTabbedPane())
+        frame.pack()
+        frame.setVisible(True)
+
+    def createTabbedPane(self):
+        class TabSelectionListener(swing.event.ChangeListener):
+            def stateChanged(listenerSelf, event):
+                pane = event.getSource()
+                index = pane.getSelectedIndex()
+                # Shouldn't be visible, unless we're actually there
+                pane.getComponentAt(0).getComponents()[0].setText("Edited")
+                print pane.getTitleAt(index), "selected"
+                
+        tPane = swing.JTabbedPane(swing.JTabbedPane.TOP)
+        self.addTabs(tPane)
+        tPane.addChangeListener(TabSelectionListener())
+        return tPane
+    
+    def addTabs(self, tabbedPane):
+        for i in range(1, 3):
+            panel = swing.JPanel()
+            panel.add(swing.JButton("Press " + str(i)))
+            tabbedPane.add("Tab " + str(i), panel)
+
+    @staticmethod            
+    def main():
+        app = TabsApp()
+        app.make_ui()
+
+TabsApp.main()
