797c3468bb76f462202ccc045a07b91b9c616ab7
Geoff Bache <geoff.bache@jeppesen.com>
2011-08-24 08:35:31 +0200

    No longer support double-clicking in editable cells, changing test accordingly

---
+++ swing/tables/select_cells/target_ui.py
@@ -0,0 +1,48 @@
+from javax import swing
+from java.awt import BorderLayout, Dimension
+from java.awt.event import MouseAdapter
+
+class MyTableModel(swing.table.DefaultTableModel):
+    def isCellEditable(self, row, col):
+        return col != 2
+
+
+class TableApp:
+        
+    def make_ui(self):
+        frame = swing.JFrame("Table demo")
+        frame.setDefaultCloseOperation(swing.JFrame.DISPOSE_ON_CLOSE)
+        frame.setLayout(BorderLayout())
+        scrollPane = swing.JScrollPane()
+        scrollPane.setPreferredSize(Dimension(300,100))
+        scrollPane.getViewport().setView(self.createTable())
+        panel = swing.JPanel()
+        panel.add(scrollPane)
+        frame.add(panel)
+        frame.pack()
+        frame.setVisible(True)
+
+    def createTable(self):
+        data = [
+                ['Anders', '21' ,'male'],
+                ['Eva', '22', 'female'],
+                ]
+        columns = ("Name", "Age", "Gender")
+        model = MyTableModel(data, columns)
+        table = swing.JTable(model)
+        table.setSelectionMode(swing.ListSelectionModel.MULTIPLE_INTERVAL_SELECTION)
+        table.setCellSelectionEnabled(True)
+        class PrintListener(MouseAdapter):
+            def mousePressed(self, e):
+                if e.getClickCount() == 2:
+                    print "Got double-click!"
+
+        table.addMouseListener(PrintListener())
+        return table
+            
+    @staticmethod            
+    def main():
+        app = TableApp()
+        app.make_ui()
+
+TableApp.main()
