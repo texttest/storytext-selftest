ee552cdbe303174c02457fe1ff55d782443045dd
Daniel Galdames <daniel.galdames@jeppesen.com>
2011-09-02 17:10:52 +0200

    Test suite for combo boxes added

---
+++ swing/combo_boxes/uneditable/target_ui.py
@@ -0,0 +1,34 @@
+from javax import swing
+from java.awt import BorderLayout
+from java.awt.event import ActionListener
+
+class JComboBoxApp:
+   
+    def make_ui(self):
+        class SelectionListener(ActionListener):
+            def actionPerformed(listenerSelf, event):
+                selected = self.cbox.selectedIndex
+                if selected >= 0:
+                    data = self.data[selected]
+                    print data + " selected"
+               
+        frame = swing.JFrame("Combo box demo")
+        frame.setSize(100, 100)
+        frame.setLayout(BorderLayout())
+
+        self.data = ("One", "Two", "Three")
+        self.cbox = swing.JComboBox(self.data)
+        self.cbox.addActionListener(SelectionListener())
+        panel = swing.JPanel()
+        panel.add(self.cbox)
+
+        frame.add(panel, BorderLayout.CENTER)
+        frame.setDefaultCloseOperation(swing.WindowConstants.DISPOSE_ON_CLOSE)
+        frame.setVisible(True)
+
+    def main():
+        app = JComboBoxApp()
+        app.make_ui()
+    main = staticmethod(main)
+
+JComboBoxApp.main()
