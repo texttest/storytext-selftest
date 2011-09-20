3bd62bf856a0c2b21a5354c554fdd728da0eea9d
Geoff Bache <geoff.bache@jeppesen.com>
2011-09-20 12:07:45 +0200

    Making sure we can correctly delay application events when they are triggered between the two clicks of a double-click

---
+++ swing/appevents/between_double_click/target_ui.py
@@ -0,0 +1,66 @@
+from javax import swing
+from java.awt import BorderLayout, Toolkit, Dimension, AWTEvent
+from java.awt.event import MouseEvent, AWTEventListener
+
+class ApplicationEvent(MouseEvent):
+    def __init__(self, component, message):
+        MouseEvent.__init__(self, component, MouseEvent.MOUSE_PRESSED, 0, 0, 0, 0, 0, False)
+        self.message = message
+
+    def getApplicationEventMessage(self):
+        return self.message
+
+class ApplicationEventManager(swing.JComponent):
+    instance = None
+    
+    def sendApplicationEvent(self, message):
+        ev = ApplicationEvent(self, message)
+        queue = Toolkit.getDefaultToolkit().getSystemEventQueue()
+        currEvents = [ ev ]
+        while queue.peekEvent():
+            currEvents.append(queue.getNextEvent())
+        for currEvent in currEvents:
+            queue.postEvent(currEvent)
+
+
+ApplicationEventManager.instance = ApplicationEventManager()
+
+class MouseReleaseListener(AWTEventListener):
+    def eventDispatched(self, event):
+        if event.getID() == MouseEvent.MOUSE_RELEASED and event.getClickCount() == 1:
+            ApplicationEventManager.instance.sendApplicationEvent("single click to be received")
+
+class TableApp:        
+    def make_ui(self):
+        frame = swing.JFrame("Table demo")
+        frame.setDefaultCloseOperation(swing.JFrame.DISPOSE_ON_CLOSE)
+        frame.setLayout(BorderLayout())
+        scrollPane = swing.JScrollPane()
+        scrollPane.setPreferredSize(Dimension(300,100))
+        scrollPane.getViewport().setView(self.createTable())
+        panel = swing.JPanel()
+        panel.add(scrollPane)
+        frame.add(panel)
+        frame.pack()
+        frame.setVisible(True)
+
+    def createTable(self):
+        class myTable(swing.JTable):
+            def isCellEditable(self, rowIndex, colIndex):
+                return False
+    
+        data = [ ['Tom'], ['Dick'], ['Harry'] ]
+        columns = ("Name",)
+        model = swing.table.DefaultTableModel(data, columns)
+        table = myTable(model)
+        table.setSelectionMode(swing.ListSelectionModel.MULTIPLE_INTERVAL_SELECTION)
+        table.setCellSelectionEnabled(True)
+        Toolkit.getDefaultToolkit().addAWTEventListener(MouseReleaseListener(), AWTEvent.MOUSE_EVENT_MASK)
+        return table
+            
+    @staticmethod            
+    def main():
+        app = TableApp()
+        app.make_ui()
+
+TableApp.main()
