f52347c61e90ed2f46fcff305fa19d4830a164af
Geoff Bache <geoff.bache@jeppesen.com>
2011-11-03 09:37:58 +0100

    Adding a little test for custom widget events working with Eclipse RCP

---
+++ swt/eclipse_rcp/jobs/customwidgetevents.py
@@ -0,0 +1,12 @@
+
+from usecase.javaswttoolkit import simulator as swtsimulator
+import org.eclipse.swtbot.swt.finder as swtbot
+
+class ButtonSelectEvent(swtsimulator.SelectEvent):
+    def _generate(self, *args):
+        # Just to make sure we can plug in this mechanism...
+        print "We faked a button click!"
+        self.widget.click()
+
+# Standard name for module containing custom widget events
+customEventTypes = [ (swtbot.widgets.SWTBotButton, [ ButtonSelectEvent ])]
