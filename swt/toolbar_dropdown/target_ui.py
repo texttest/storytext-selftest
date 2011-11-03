e6eb386fa895883c620ef2175acc15db0da5c515
Geoff Bache <geoff.bache@jeppesen.com>
2011-11-03 17:10:16 +0100

    Support drop-down tool items

---
+++ swt/toolbar_dropdown/target_ui.py
@@ -0,0 +1,45 @@
+from org.eclipse.swt import *
+from org.eclipse.swt.widgets import *
+from org.eclipse.swt.graphics import *
+
+def addItem(i, menu):
+    item = MenuItem(menu, SWT.PUSH)
+    item.setText("Item " + str(i))
+    class PrintListener(Listener):
+        def handleEvent(self, e):
+            print "Selected item", i
+            
+    item.addListener(SWT.Selection, PrintListener())
+
+shell = Shell()
+
+toolBar = ToolBar(shell, SWT.NONE)
+
+clientArea = shell.getClientArea()
+toolBar.setLocation(clientArea.x, clientArea.y)
+menu = Menu(shell, SWT.POP_UP)
+for i in range(8):
+    addItem(i, menu)
+
+item = ToolItem(toolBar, SWT.DROP_DOWN)
+item.setText("Drop Down")
+class MenuListener(Listener):
+    def handleEvent(self, event):
+        rect = item.getBounds()
+        pt = Point(rect.x, rect.y + rect.height)
+        pt = toolBar.toDisplay(pt)
+        menu.setLocation(pt.x, pt.y)
+        menu.setVisible(True)
+
+
+item.addListener(SWT.Selection, MenuListener())
+toolBar.pack()
+shell.pack()
+shell.setSize(100, 100)
+shell.open()
+display = shell.getDisplay()
+while not shell.isDisposed():
+    if not display.readAndDispatch():
+        display.sleep()
+        
+display.dispose()
