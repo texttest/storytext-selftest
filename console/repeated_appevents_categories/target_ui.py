ae4683dde10647bcbe025eee2975918227e644c3
Geoff Bache <geoff.bache@jeppesen.com>
2011-12-07 15:28:51 +0100

    Handle repeated events in categories also

---
+++ console/repeated_appevents_categories/target_ui.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from storytext import applicationEvent
+import time
+
+def sleep(length):
+    try:
+        time.sleep(length)
+    except KeyboardInterrupt:
+        print "Interrupted while sleeping for", length, "seconds."
+
+sleep(0.5)
+applicationEvent("the event", "main")
+applicationEvent("the other event", "other")
+applicationEvent("the event", "main")
+sleep(5)
