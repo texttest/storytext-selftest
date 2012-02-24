48bdbc52e66251a6866d8d3f5e8c7d1ec94612b1
Geoff Bache <geoff.bache@jeppesen.com>
2012-02-24 16:58:55 +0100

    Make sure we handle application events correctly when they're the last thing to be delayed

---
+++ console/appevent_delayed/target_ui.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from storytext import applicationEvent
+import time, signal, sys
+
+def handleSignal(signum, *args):
+    sys.stderr.write("Got signal " + repr(signum) + "\n")
+
+signal.signal(signal.SIGQUIT, handleSignal)
+signal.signal(signal.SIGINT, handleSignal)
+
+time.sleep(0.5)
+applicationEvent("first sleep to complete", "first", delayLevel=1)
+time.sleep(5)
+time.sleep(0.5)
+applicationEvent("second sleep to complete", "second")
+time.sleep(5)
