d2c59aeba4f94ec2ac6cc7c273dd9288243da1f7
Geoff Bache <geoff.bache@jeppesen.com>
2012-05-16 14:57:49 +0200

    Test we can handle repeated shortcuts ending in an appevent

---
+++ console/appevent_repeated_shortcut/target_ui.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from storytext import applicationEvent
+import time, signal, sys
+
+def handleSignal(signum, *args):
+    sys.stderr.write("Got signal " + repr(signum) + "\n")
+
+signal.signal(signal.SIGQUIT, handleSignal)
+signal.signal(signal.SIGINT, handleSignal)
+
+time.sleep(0.5)
+applicationEvent("first sleep to complete", category="sleeps")
+time.sleep(5)
+time.sleep(0.5)
+applicationEvent("normal sleep to complete", category="sleeps")
+time.sleep(5)
+time.sleep(0.5)
+applicationEvent("normal sleep to complete", category="sleeps")
+time.sleep(5)
