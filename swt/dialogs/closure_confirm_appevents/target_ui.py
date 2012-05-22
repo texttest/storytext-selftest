4f36296ee2d9b103d4a2e02d67b5d67e1b770048
Geoff Bache <geoff.bache@jeppesen.com>
2012-05-22 15:12:08 +0200

    New test for interplay of state-changes, app events and delay levels...

---
+++ swt/dialogs/closure_confirm_appevents/target_ui.py
@@ -0,0 +1,69 @@
+from org.eclipse.swt import *
+from org.eclipse.swt.widgets import *
+from org.eclipse.swt.layout import *
+from java.lang import Runnable
+
+
+shell = Shell()
+
+class ConfirmListener(Listener):
+    def handleEvent(self, e):
+        style = SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL
+        messageBox = Shell(shell, style)
+        messageBox.setText("Information")
+        layout = GridLayout()
+        messageBox.setLayout(layout)
+        label = Label(messageBox, SWT.NONE)
+        label.setText("You're exiting!")
+        button = Button(messageBox, SWT.PUSH)
+        button.setText("OK")
+        class CloseListener(Listener):
+            def handleEvent(self, e):
+                messageBox.close()
+        button.addListener(SWT.Selection, CloseListener())
+        messageBox.pack()
+        messageBox.open()
+        display = messageBox.getDisplay()
+        while not messageBox.isDisposed():
+            if not display.readAndDispatch():
+                display.sleep()
+
+shell.setText("Main Window")
+shell.setLayout(FillLayout())
+
+composite = Composite(shell, SWT.NONE)
+layout = GridLayout(2, False)
+composite.setLayout(layout)
+
+label = Label(composite, SWT.NONE)
+label.setText("Name")
+text = Text(composite, SWT.NONE)
+label2 = Label(composite, SWT.NONE)
+label2.setText("City")
+text2 = Text(composite, SWT.NONE)
+
+class AppEventRunnable(Runnable):
+    def run(self):
+        x = Event()
+        x.text = "text update to complete"
+        text.notifyListeners(1234, x)
+
+
+class ModifyListener(Listener):
+    def handleEvent(self, e):
+        if e.widget.getText() == "xxx":
+            shell.getDisplay().timerExec(1000, AppEventRunnable())
+
+text.addListener(SWT.Modify, ModifyListener())
+text2.addListener(SWT.Modify, ModifyListener())
+
+shell.addListener(SWT.Close, ConfirmListener())
+
+shell.setSize(200, 200)
+shell.open()
+display = shell.getDisplay()
+while not shell.isDisposed():
+    if not display.readAndDispatch():
+        display.sleep()
+        
+display.dispose()
