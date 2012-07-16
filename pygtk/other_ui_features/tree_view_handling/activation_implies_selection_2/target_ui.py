54a9da2d82d92429b9dfb4bb338bd7f89e2b4159
Geoff Bache <geoff.bache@jeppesen.com>
2012-07-16 16:28:58 +0200

    New test for another app event corner case

---
+++ pygtk/other_ui_features/tree_view_handling/activation_implies_selection_2/target_ui.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+# example basictreeview.py
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+from storytext import applicationEvent
+from threading import Timer
+
+class BasicTreeViewExample:
+
+    # close the window and quit
+    def delete_event(self, widget, event, data=None):
+        gtk.main_quit()
+        return False
+
+    def __init__(self):
+        # Create a new window
+        self.appEventsEnabled = True
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+
+        self.window.set_title("Basic TreeView Example")
+
+        self.window.set_size_request(400, 400)
+
+        self.window.connect("delete_event", self.delete_event)
+
+        # create a TreeStore with one string column to use as the model
+        self.treestore = gtk.TreeStore(str)
+
+        self.treestore.append(None, [ "leaf" ])
+
+        # create the TreeView using treestore
+        self.treeview = gtk.TreeView(self.treestore)
+
+        # create the TreeViewColumn to display the data
+        self.tvcolumn = gtk.TreeViewColumn('Column 0')
+
+        # add tvcolumn to treeview
+        self.treeview.append_column(self.tvcolumn)
+
+        # create a CellRendererText to render the data
+        self.cell = gtk.CellRendererText()
+
+        # add the cell to the tvcolumn and allow it to expand
+        self.tvcolumn.pack_start(self.cell, True)
+
+        # set the cell "markup" attribute to column 0 - retrieve marked-up text
+        # from that column in treestore
+        self.tvcolumn.add_attribute(self.cell, 'text', 0)
+
+        # make it searchable
+        self.treeview.get_selection().set_mode(gtk.SELECTION_MULTIPLE)
+        self.treeview.get_selection().connect("changed", self.selection_changed)
+        self.treeview.expand_all()
+
+        self.window.add(self.treeview)
+
+        self.window.show_all()
+   
+    def selection_changed(self, selection, *args):
+        if self.appEventsEnabled:
+            Timer(1, applicationEvent, [ "selection to be changed" ]).start()
+            self.appEventsEnabled = False
+
+        
+
+def main():
+    gtk.main()
+
+if __name__ == "__main__":
+    tvexample = BasicTreeViewExample()
+    main()
