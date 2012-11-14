07c33aa1ed961822c85c62fe352fe434c27cfa9c
Geoff Bache <geoff.bache@jeppesen.com>
2012-11-14 15:15:02 +0100

    New test for indentation, ignoring script output etc

---
+++ swt/browsers/table_indent/target_ui.py
@@ -0,0 +1,39 @@
+## /*******************************************************************************
+##  * Copyright (c) 2000, 2004 IBM Corporation and others.
+##  * All rights reserved. This program and the accompanying materials
+##  * are made available under the terms of the Eclipse Public License v1.0
+##  * which accompanies this distribution, and is available at
+##  * http://www.eclipse.org/legal/epl-v10.html
+##  *
+##  * Contributors:
+##  *     IBM Corporation - initial API and implementation
+##  *******************************************************************************/
+## /*
+##  * Tree example snippet: create a tree
+##  *
+##  * For a list of all SWT example snippets see
+##  * http://www.eclipse.org/swt/snippets/
+## */
+
+import os
+from org.eclipse.swt import *
+from org.eclipse.swt.widgets import *
+from org.eclipse.swt.layout import *
+from org.eclipse.swt.browser import *
+
+display = Display()
+shell = Shell(display)
+shell.setLayout(FillLayout())
+
+browser = Browser(shell, SWT.None)
+fileName = "file://" + os.path.abspath("target_ui.html")
+browser.setUrl(fileName);
+
+shell.pack()
+shell.setSize(500, 500)
+shell.open()
+while not shell.isDisposed():
+    if not display.readAndDispatch():
+        display.sleep()
+
+display.dispose()
