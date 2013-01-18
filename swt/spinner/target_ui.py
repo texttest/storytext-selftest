a228c11cb71f96aed740eaccba9ca8ede627ae52
Geoff Bache <geoff.bache@jeppesen.com>
2013-01-18 13:28:14 +0100

    Adding test for Spinner

---
+++ swt/spinner/target_ui.py
@@ -0,0 +1,30 @@
+from org.eclipse.swt import *
+from org.eclipse.swt.widgets import *
+from org.eclipse.swt.layout import *
+
+shell = Shell()
+
+layout = GridLayout(2, False)
+shell.setLayout(layout)
+
+label = Label(shell, SWT.NONE)
+label.setText("The Spinner")
+
+spinner = Spinner(shell, SWT.BORDER)
+spinner.setMinimum(0)
+spinner.setMaximum(1000)
+spinner.setSelection(500)
+spinner.setIncrement(1)
+spinner.setPageIncrement(100)
+
+label.pack()
+spinner.pack()
+		
+shell.pack()
+shell.open()
+display = shell.getDisplay()
+while not shell.isDisposed():
+    if not display.readAndDispatch():
+        display.sleep()
+        
+display.dispose()
