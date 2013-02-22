a201a7afcca7cd1d22d101f81bd8f6268b40e1e8
Geoff Bache <geoff.bache@jeppesen.com>
2013-02-22 12:25:24 +0100

    New test for TTT-3038

---
+++ swt/tooltip_label_conflict/target_ui.py
@@ -0,0 +1,37 @@
+from org.eclipse.swt import *
+from org.eclipse.swt.widgets import *
+from org.eclipse.swt.layout import *
+
+shell = Shell()
+image = shell.getDisplay().getSystemImage(SWT.ICON_QUESTION)
+layout = GridLayout(2, False)
+shell.setLayout(layout)
+button = Button(shell, SWT.PUSH)
+button.setImage(image)
+button.setToolTipText("The Ultimate Question")
+button.setText("Why?")
+
+class PrintListener(Listener):
+    def handleEvent(self, e):
+        print "Because."
+
+button.addListener(SWT.Selection, PrintListener())
+
+button2 = Button(shell, SWT.PUSH)
+button2.setText("Exit")
+
+class CloseListener(Listener):
+    def handleEvent(self, e):
+        shell.close()
+
+
+button2.addListener(SWT.Selection, CloseListener())
+
+shell.setSize(200, 200)
+shell.open()
+display = shell.getDisplay()
+while not shell.isDisposed():
+    if not display.readAndDispatch():
+        display.sleep()
+        
+display.dispose()
