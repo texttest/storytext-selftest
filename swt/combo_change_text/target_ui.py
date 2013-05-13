0d14ea94a9123840df5ef2f373fc326b1ad60fca
Geoff Bache <geoff.bache@jeppesen.com>
2013-05-13 09:44:47 +0200

    TTT-3126, better handling of Combo selections

---
+++ swt/combo_change_text/target_ui.py
@@ -0,0 +1,27 @@
+from org.eclipse.swt import *
+from org.eclipse.swt.widgets import *
+from org.eclipse.swt.layout import *
+
+shell = Shell()
+combo = Combo(shell, SWT.READ_ONLY)
+combo.setItems([ "Alpha", "Bravo", "Charlie" ])
+combo.setText("Alpha")
+combo.setSize(100, 10)
+
+class PrintListener(Listener):
+    def handleEvent(self, e):
+        ix = combo.getSelectionIndex()
+        newText = "Selected " + combo.getItem(ix)
+        combo.setItem(ix, newText)
+        combo.setText(newText)
+
+combo.addListener(SWT.Selection, PrintListener())
+
+shell.pack()
+shell.open()
+display = shell.getDisplay()
+while not shell.isDisposed():
+    if not display.readAndDispatch():
+        display.sleep()
+        
+display.dispose()
