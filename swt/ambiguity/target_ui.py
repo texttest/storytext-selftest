e1e40577f9ee674e242346f366cd21d6de761717
Geoff Bache <geoff.bache@jeppesen.com>
2013-05-20 16:02:09 +0200

    TTT-3140: ambiguity handling improved

---
+++ swt/ambiguity/target_ui.py
@@ -0,0 +1,37 @@
+from org.eclipse.swt import *
+from org.eclipse.swt.widgets import *
+from org.eclipse.swt.layout import *
+
+shell = Shell()
+image = shell.getDisplay().getSystemImage(SWT.ICON_WARNING)
+layout = GridLayout(2, False)
+shell.setLayout(layout)
+button = Button(shell, SWT.PUSH)
+button.setImage(image)
+button.setToolTipText("Has an image!")
+button.setText("Button")
+
+class PreferredPrintListener(Listener):
+    def handleEvent(self, e):
+        print "Right!"
+
+button.addListener(SWT.Selection, PreferredPrintListener())
+
+button2 = Button(shell, SWT.PUSH)
+button2.setText("Button")
+button2.setToolTipText("Has no image!")
+
+class PrintListener(Listener):
+    def handleEvent(self, e):
+        print "Wrong!"
+
+button2.addListener(SWT.Selection, PrintListener())
+
+shell.setSize(200, 200)
+shell.open()
+display = shell.getDisplay()
+while not shell.isDisposed():
+    if not display.readAndDispatch():
+        display.sleep()
+        
+display.dispose()
