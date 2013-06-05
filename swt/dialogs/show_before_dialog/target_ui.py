d79ab2ec0a13c5a7fff7d5b2e3062e5fc53acce5
Geoff Bache <geoff.bache@jeppesen.com>
2013-06-05 14:55:27 +0200

    TTT-3160: don't monitor widgets if they're shown before the shell they appear in!

---
+++ swt/dialogs/show_before_dialog/target_ui.py
@@ -0,0 +1,40 @@
+from org.eclipse.swt import *
+from org.eclipse.swt.widgets import *
+from org.eclipse.swt.layout import *
+
+shell = Shell()
+
+class ConfirmListener(Listener):
+    def handleEvent(self, e):
+        style = SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL
+        messageBox = Shell(shell, style)
+        layout = GridLayout()
+        messageBox.setLayout(layout)
+        label = Label(messageBox, SWT.NONE)
+        label.setText("You're exiting!")
+        button = Button(messageBox, SWT.PUSH)
+        button.setText("OK")
+        class CloseListener(Listener):
+            def handleEvent(self, e):
+                messageBox.close()
+        button.addListener(SWT.Selection, CloseListener())
+        messageBox.pack()
+        button.notifyListeners(SWT.Show, Event())
+        messageBox.setText("Information")
+        messageBox.open()
+        display = messageBox.getDisplay()
+        while not messageBox.isDisposed():
+            if not display.readAndDispatch():
+                display.sleep()
+
+shell.setText("Main Window")
+shell.addListener(SWT.Close, ConfirmListener())
+
+shell.setSize(200, 200)
+shell.open()
+display = shell.getDisplay()
+while not shell.isDisposed():
+    if not display.readAndDispatch():
+        display.sleep()
+        
+display.dispose()
