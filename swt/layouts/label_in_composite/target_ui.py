1c9077c72400f2f64d868d8221716acf41069ee6
Geoff Bache <geoff.bache@jeppesen.com>
2013-06-07 13:59:48 +0200

    TTT-3162: make sure we use the last label of composites, if appropriate

---
+++ swt/layouts/label_in_composite/target_ui.py
@@ -0,0 +1,74 @@
+## /*******************************************************************************
+##  * Copyright (c) 2000, 2004 IBM Corporation and others.
+##  * All rights reserved. This program and the accompanying materials
+##  * are made available under the terms of the Eclipse Public License v1.0
+##  * which accompanies this distribution, and is available at
+##  * http://www.eclipse.org/legal/epl-v10.html
+##  *
+##  * Contributors:
+##  *     IBM Corporation - initial API and implementation
+##  *******************************************************************************/
+## /*
+##  * Tree example snippet: create a tree
+##  *
+##  * For a list of all SWT example snippets see
+##  * http://www.eclipse.org/swt/snippets/
+## */
+from org.eclipse.swt import *
+from org.eclipse.swt.widgets import *
+from org.eclipse.swt.layout import *
+import locale
+encoding = locale.getdefaultlocale()[1]
+
+display = Display()
+shell = Shell(display)
+shell.setLayout(FillLayout())
+
+bar = Menu(shell, SWT.BAR)
+shell.setMenuBar(bar)
+fileItem = MenuItem(bar, SWT.CASCADE)
+fileItem.setText("&File")
+submenu = Menu(shell, SWT.DROP_DOWN)
+fileItem.setMenu(submenu)
+item = MenuItem(submenu, SWT.PUSH)
+item.setText("Describe")
+
+composite = Composite(shell, SWT.NONE)
+layout = GridLayout(2, False)
+composite.setLayout(layout)
+
+label = Label(composite, SWT.NONE)
+label.setText("Name")
+text = Text(composite, SWT.NONE)
+
+middleComposite = Composite(composite, SWT.NONE)
+layout = GridLayout(1, False)
+middleComposite.setLayout(layout)
+
+label2Composite = Composite(middleComposite, SWT.NONE)
+layout = GridLayout(2, False)
+label2Composite.setLayout(layout)
+
+label2 = Label(label2Composite, SWT.NONE)
+label2.setText("Home")
+
+label2 = Label(label2Composite, SWT.NONE)
+label2.setText("City")
+text2 = Text(composite, SWT.NONE)
+
+
+class PrintListener(Listener):
+    def handleEvent(self, e):
+        unicodeText = "You are " + text.getText() + " and you live in " + text2.getText()
+        print unicodeText.encode(encoding, 'replace')
+        
+item.addListener(SWT.Selection, PrintListener())
+
+
+shell.setSize(200, 200)
+shell.open()
+while not shell.isDisposed():
+    if not display.readAndDispatch():
+        display.sleep()
+
+display.dispose()
