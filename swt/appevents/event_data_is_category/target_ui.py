a03b6e64318256d3626236363aad8834598b776e
Geoff Bache <geoff.bache@jeppesen.com>
2013-11-01 14:00:13 +0100

    Test we can use event.data as a category

---
+++ swt/appevents/event_data_is_category/target_ui.py
@@ -0,0 +1,67 @@
+## /*******************************************************************************
+##  * Copyright (c) 2000, 2004 IBM Corporation and others.
+##  * All rights reserved. This program and the accompanying materials
+##  * are made available under the terms of the Eclipse Public License v1.0
+##  * which accompanies this distribution, and is available at
+##  * http://www.eclipse.org/legal/epl-v10.html
+##  *
+##  * Contributors:
+##  *     IBM Corporation - initial API and implementation
+##  *******************************************************************************/
+## /*
+##  * Tree example snippet: create a tree
+##  *
+##  * For a list of all SWT example snippets see
+##  * http://www.eclipse.org/swt/snippets/
+## */
+from org.eclipse.swt import *
+from org.eclipse.swt.widgets import *
+from org.eclipse.swt.layout import *
+from java.lang import Runnable
+
+display = Display()
+shell = Shell(display)
+shell.setLayout(FillLayout())
+
+tree = Tree(shell, SWT.BORDER | SWT.MULTI)
+for i in range(4):
+    iItem = TreeItem(tree, 0)
+    iItem.setText("Initial - " + str(i))
+
+
+def sendAppEvent(text, category=None):
+     x = Event()
+     x.text = text
+     if category:
+         x.data = category
+     tree.notifyListeners(1234, x)
+
+
+class TreeAddRunnable(Runnable):
+    def run(self):
+        if tree.isDisposed():
+            return
+        item = TreeItem(tree, 0)
+        item.setText("Added - 0")
+        sendAppEvent("a different event", "different")
+        sendAppEvent("rows to be added")
+
+class AppEventRunnable(Runnable):
+    def run(self):
+        sendAppEvent("rows to be added")
+
+
+class AddListener(Listener):
+    def handleEvent(self, e):
+        display.timerExec(1000, TreeAddRunnable())
+
+tree.addListener(SWT.Selection, AddListener())
+
+display.timerExec(2000, AppEventRunnable())
+shell.setSize(200, 200)
+shell.open()
+while not shell.isDisposed():
+    if not display.readAndDispatch():
+        display.sleep()
+
+display.dispose()
