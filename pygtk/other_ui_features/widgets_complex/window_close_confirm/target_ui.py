92979b901e9229845850502d728a1388028e3553
Geoff Bache <geoff.bache@jeppesen.com>
2013-12-20 11:44:33 +0100

    TTT-3357: Closing windows can be intercepted and stopped by a confirmation dialog. In that case, don't try to destroy it!

---
+++ pygtk/other_ui_features/widgets_complex/window_close_confirm/target_ui.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import gtk
+class ConfirmDialog:
+    def __init__(self, parent_window):
+        self._parent_window = parent_window
+
+    def askForConfirm(self):
+        dialog = gtk.Dialog('Confirm', self._parent_window)
+        text = "Are you sure you want to close the window?"
+        dialog.vbox.add(gtk.Label(text))
+        
+        dialog.add_button('Yes', gtk.RESPONSE_YES)
+        dialog.add_button('No', gtk.RESPONSE_NO)
+        dialog.show_all()
+        result = dialog.run()
+        ret = result == gtk.RESPONSE_YES
+        dialog.hide()
+        dialog.destroy()
+        return ret
+        
+
+if __name__ == "__main__":
+    win = gtk.Window()
+
+    def close(*args):
+        dialog = ConfirmDialog(win)
+        if dialog.askForConfirm():
+            gtk.main_quit()
+        else:
+            return not win.emit_stop_by_name("delete_event")
+
+    win.set_deletable(True)
+
+    win.connect("delete-event", close)
+
+    win.show_all()
+    gtk.main()
